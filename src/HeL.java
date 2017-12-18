c3a8975503366c6959fc66938c022994ea9d8413
benjaminbylund <te16.benjamin.bylund@umea.nti.se>
2017-12-18 08:57:25 +0100

    Slutförde spelet

--- src/HeL.java
+++ src/HeL.java
@@ -3,39 +3,68 @@ import java.util.Scanner;
 
 public class HeL {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int Nummer = (int) (Math.random() * 13);
-        System.out.println(Nummer);
-
-        System.out.println("Gissa om nästa nummer är högre eller lägre.");
-
-        String Gissning = sc.nextLine();
-        Gissning = Gissning.toUpperCase();
-
-
-        int Nummer1 = (int) (Math.random() * 13);
-        System.out.println(Nummer1);
-
+        System.out.println(
+                "*-------------*\n" +
+                "|  \\|/        |\n" +
+                "| M-O-RNING   |\n" +
+                "|  /|\\8=WOO=D |\n" +
+                "| INTERACTIVE |\n" +
+                "*-------------*");
+        boolean lp = true;
         boolean sp = true;
+        int streak = 0;
 
-         while ( sp )  {
-            if (Gissning.equals("HÖGRE")) {
-                if (Nummer < Nummer1) {
-                    System.out.println("Fortsätt");
-                } else if (Nummer > Nummer1) {
-                    System.out.println("Du förlorade, testa igen.");
-                    sp = false;
+        while (lp) {
+            Scanner sc = new Scanner(System.in);
+            int nummer = (int) (Math.random() * 13);
+            System.out.println(nummer);
+
+            System.out.println("Gissa om nästa nummer är högre eller lägre.");
+
+            String gissning = sc.nextLine();
+            gissning = gissning.toUpperCase();
+
+            int nummer1 = (int) (Math.random() * 13);
+            System.out.println(nummer1);
+
+
+            while (sp) {
+
+                if (gissning.equals("HÖGRE")) {
+                    if (nummer > nummer1) {
+                        System.out.println("FÖRLUST -25 mmr");
+                        sp = false;
+                        lp = false;
+                    } else {
+                        System.out.println("VINST");
+                        streak = streak + 1;
+                        System.out.println("streak:" + streak );
+                        break;
+
+                       /* System.out.println("Vill du fortsätta eller börja om?");
+                        Scanner sc1 = new Scanner(System.in);
+                        String feb = sc1.nextLine();
+
+                            if (feb.equals ("Fortsätta")){
+
+                            }*/
+                    }
                 }
-            } else {//if (Gissning.equals("LÄGRE")) {
-                if (Nummer > Nummer1) {
-                    System.out.println("Fortsätt");
-                } else if (Nummer < Nummer1) {
-                    System.out.println("Du förlorade, testa igen.");
-                    sp = false;
-                }
+                if (gissning.equals("LÄGRE")) {
+                    if (nummer < nummer1) {
+                        System.out.println("FÖRLUST -25 mmr");
+                        sp = false;
+                        lp = false;
+                    } else {
+                        System.out.println("VINST");
+                        streak = streak + 1;
+                        System.out.println("streak:" + streak);
+                        break;
+                    }
+                } //else {
+                //System.out.println("lägg ner det här är allvar /votekick");
+                //}
             }
         }
     }
 }
-
